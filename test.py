3e4b5a95d296acb970ff83a8dd5f3dba15e5000e
ozmen68 <menozmen68@gmail.com>
2026-03-11 23:41:41 +0300

    Update Telegram API URL with chat_id and text

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 TOKEN = "8334170823:AAEV3aTrgoJHSjsNq5qybt7sszdKiLrhxg0"
 CHAT_ID = "-1003736768920"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+url = f"https://api.telegram.org/bot8334170823:AAEV3aTrgoJHSjsNq5qybt7sszdKiLrhxg0/sendMessage?chat_id=-1003736768920&text=Test%20mesaji"
 
 requests.post(url, data={
     "chat_id": CHAT_ID,
